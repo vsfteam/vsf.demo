767bb35a38eee24af1c93dbe6587e8def94b708b
vsfos <vsfos@foxmail.com>
2023-01-07 20:29:46 +0800

    [application/uac] simplify

--- application/component/usb/device/class/uac/main.c
+++ application/component/usb/device/class/uac/main.c
@@ -92,11 +92,8 @@ typedef struct usbd_uac_t {
 /*============================ GLOBAL VARIABLES ==============================*/
 /*============================ LOCAL VARIABLES ===============================*/
 
-describe_mem_stream(__user_usbd_uac_rx_stream, 192)
+describe_mem_stream(__user_usbd_uac_rx_stream, 2 * 192)
 describe_mem_stream(__user_usbd_uac_tx_stream, 96)
-
-describe_mem_stream(__audio_ticktock_stream, 2 * 192)
-describe_stream_adapter(__audio_stream_adapter, &__user_usbd_uac_rx_stream, &__audio_ticktock_stream)
 
 static const usbd_uac_const_t __user_usbd_uac_const = {
     .usbd                       = {
@@ -514,16 +511,13 @@ int VSF_USER_ENTRY(void)
     vsf_board_init();
     vsf_start_trace();
 
-    vsf_stream_init(&__audio_ticktock_stream.use_as__vsf_stream_t);
+    vsf_stream_init(&__user_usbd_uac_rx_stream.use_as__vsf_stream_t);
     vk_audio_init(vsf_board.audio_dev);
-    vk_audio_start(vsf_board.audio_dev, 0, &__audio_ticktock_stream.use_as__vsf_stream_t, &(vk_audio_format_t){
+    vk_audio_start(vsf_board.audio_dev, 0, &__user_usbd_uac_rx_stream.use_as__vsf_stream_t, &(vk_audio_format_t){
         .datatype.value     = VSF_AUDIO_DATA_TYPE_LEU16,
         .sample_rate        = 480,
         .channel_num        = 2,
     });
-
-    vsf_stream_init(&__user_usbd_uac_rx_stream.use_as__vsf_stream_t);
-    vsf_stream_adapter_init(&__audio_stream_adapter);
 
     vsf_stream_t *stream = &__user_usbd_uac_tx_stream.use_as__vsf_stream_t;
     vsf_stream_init(stream);
