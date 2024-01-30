259524d30da602c087375282ccf042918095112d
vsfos <vsfos@foxmail.com>
2024-01-30 15:53:10 +0800

    [application/linux/applets] prepare to add curl

---
+++ application/shell/sys/linux/applets/curl/README.md
@@ -0,0 +1,21 @@
+# curl applet
+
+## Submodule
+- vsf
+  - PLOOC
+- application/shell/sys/linux/applets/curl/raw
+
+## patch
+
+## Compiler and Utilities
+- arm-none-eabi-gcc: https://developer.arm.com/downloads/-/arm-gnu-toolchain-downloads
+- cmake: https://github.com/Kitware/CMake/releases
+- ninja: https://github.com/ninja-build/ninja/releases
+
+## Commands(current directory is application/shell/sys/linux/applets/curl)
+```
+> mkdir build
+> cd build
+> cmake -G Ninja ..
+> ninja
+```
---
+++ application/shell/sys/linux/applets/curl/curl.cmake
@@ -0,0 +1,29 @@
+# CMakeLists head
+
+cmake_policy(SET CMP0079 NEW)
+
+file(GLOB CURL_SOURCES
+    raw/lib/vauth/*.c
+    raw/lib/vquit/*.c
+    raw/lib/vssh/*.c
+    raw/lib/vtls/*.c
+    raw/lib/*.c
+    raw/src/*.c
+)
+vsf_add_compile_definitions(
+    ${VSF_TARGET_DEFINITIONS}
+    __VSF__
+
+    HAVE_CONFIG_H
+)
+vsf_add_include_directories(
+    port
+    raw/include
+    raw/lib
+
+    $ENV{VSF_PATH}/source/component/3rd-party/mbedtls/raw/include
+    ${VSF_COMMON_INCLUDE_DIRECTORIES}
+)
+vsf_add_sources(
+    ${CURL_SOURCES}
+)
