981c0bd0dd03476ae83d2232a8084e3b1b648b15
vsfos <vsfos@foxmail.com>
2024-02-17 17:46:41 +0800

    [application/linux/font] call vsf_linux_loader_keep_ram to keep ram when exit

--- application/shell/sys/linux/applets/font/font_vplt.c
+++ application/shell/sys/linux/applets/font/font_vplt.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 #include <linux/font.h>
 
-extern struct font_desc font_vga_8x8, font_vga_8x16;
+extern const struct font_desc font_vga_8x8, font_vga_8x16;
 
-__VSF_VPLT_DECORATOR__ vsf_linux_font_vplt_t vsf_linux_font_vplt = {
+static const vsf_linux_font_vplt_t __vsf_linux_font_vplt = {
     VSF_APPLET_VPLT_INFO(vsf_linux_font_vplt_t, 0, 0, true),
-    VSF_APPLET_VPLT_ENTRY_MOD(linux_font, vsf_linux_font_vplt),
+    VSF_APPLET_VPLT_ENTRY_MOD(linux_font, __vsf_linux_font_vplt),
 
     VSF_APPLET_VPLT_ENTRY_VAR(font_vga_8x8),
     VSF_APPLET_VPLT_ENTRY_VAR(font_vga_8x16),
@@ -13,6 +13,7 @@ __VSF_VPLT_DECORATOR__ vsf_linux_font_vplt_t vsf_linux_font_vplt = {
 
 int main(int argc, char **argv)
 {
-    vsf_vplt_load_dyn((vsf_vplt_info_t *)&vsf_linux_font_vplt.info);
+    vsf_vplt_load_dyn((vsf_vplt_info_t *)&__vsf_linux_font_vplt.info);
+    vsf_linux_loader_keep_ram();
     return 0;
 }
