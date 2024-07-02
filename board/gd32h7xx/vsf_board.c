f7b3ad1614f1a3d80b3639a4235ab05eedf5613f
vsfos <vsfos@foxmail.com>
2024-07-02 11:02:09 +0800

    [board][project] prepare to add gd32h759 support

---
+++ board/gd32h7xx/vsf_board.c
@@ -0,0 +1,167 @@
+/*****************************************************************************
+ *   Copyright(C)2009-2022 by VSF Team                                       *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *     http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ ****************************************************************************/
+
+/*============================ INCLUDES ======================================*/
+
+#define __VSF_SIMPLE_STREAM_CLASS_INHERIT__
+#include "./vsf_board.h"
+
+/*============================ MACROS ========================================*/
+/*============================ MACROFIED FUNCTIONS ===========================*/
+/*============================ TYPES =========================================*/
+/*============================ PROTOTYPES ====================================*/
+/*============================ LOCAL VARIABLES ===============================*/
+
+#if VSF_USE_USB_HOST == ENABLED
+static const vk_dwcotg_hcd_param_t __dwcotg_hcd_param = {
+    .op                         = &VSF_USB_HC0_IP,
+    .priority                   = APP_CFG_USBH_ARCH_PRIO,
+};
+#endif
+
+/*============================ GLOBAL VARIABLES ==============================*/
+
+#if VSF_USE_USB_DEVICE == ENABLED
+static const vk_dwcotg_dcd_param_t __dwcotg_dcd_param = {
+    .op                         = &VSF_USB_DC0_IP,
+    .speed                      = VSF_USBD_CFG_SPEED,
+        .ulpi_en                = true,
+        .dma_en                 = false,
+};
+#endif
+
+#if VSF_USE_BOARD == ENABLED
+vsf_board_t vsf_board = {
+#if VSF_HAL_USE_I2C == ENABLED
+    .i2c                        = (vsf_i2c_t *)&vsf_hw_i2c0,
+#endif
+#if VSF_USE_USB_HOST == ENABLED
+    .usbh_dev                   = {
+        .drv                    = &vk_dwcotg_hcd_drv,
+        .param                  = (void *)&__dwcotg_hcd_param,
+    },
+#endif
+#if VSF_USE_USB_DEVICE == ENABLED
+    .dwcotg_dcd                 = {
+        .param                  = &__dwcotg_dcd_param,
+    },
+#endif
+};
+#endif
+
+/*============================ LOCAL VARIABLES ===============================*/
+/*============================ IMPLEMENTATION ================================*/
+
+#if VSF_USE_USB_DEVICE == ENABLED
+vsf_usb_dc_from_dwcotg_ip(0, vsf_board.dwcotg_dcd, VSF_USB_DC0)
+#endif
+
+#if     (!defined(VSF_HAL_USE_DEBUG_STREAM) || VSF_HAL_USE_DEBUG_STREAM == DISABLED)\
+    &&  VSF_HAL_USE_USART == ENABLED
+#   define __VSF_BOARD_USE_UART_AS_DEBUG_STREAM
+#   ifndef VSF_DEBUG_STREAM_CFG_RX_BUF_SIZE
+#       define VSF_DEBUG_STREAM_CFG_RX_BUF_SIZE         32
+#   endif
+static uint8_t __vsf_debug_stream_rx_buff[VSF_DEBUG_STREAM_CFG_RX_BUF_SIZE];
+vsf_mem_stream_t VSF_DEBUG_STREAM_RX = {
+    .op         = &vsf_mem_stream_op,
+    .buffer     = __vsf_debug_stream_rx_buff,
+    .size       = sizeof(__vsf_debug_stream_rx_buff),
+};
+
+static void __vsf_debug_stream_isrhandler(void *target, vsf_usart_t *uart,
+        vsf_usart_irq_mask_t irq_mask)
+{
+    vsf_stream_t *stream = target;
+    uint8_t *buffer;
+    uint_fast32_t buflen;
+
+#if 0
+    while (vsf_usart_rxfifo_get_data_count(uart) > 0) {
+        buflen = vsf_stream_get_wbuf(stream, &buffer);
+        if (!buflen) {
+            vsf_trace_error("debug stream overflow, please increate VSF_DEBUG_STREAM_CFG_RX_BUF_SIZE\n");
+            break;
+        } else {
+            vsf_stream_write(stream, NULL, vsf_usart_rxfifo_read(uart, buffer, buflen));
+        }
+    }
+#endif
+}
+
+static void __VSF_DEBUG_STREAM_TX_INIT(void)
+{
+#if 0
+    vsf_usart_t *debug_usart = (vsf_usart_t *)&vsf_hw_usart1;
+    vsf_err_t err;
+
+    vsf_stream_connect_tx(&VSF_DEBUG_STREAM_RX.use_as__vsf_stream_t);
+    err = vsf_usart_init(debug_usart, &(vsf_usart_cfg_t){
+        .mode               = VSF_USART_8_BIT_LENGTH | VSF_USART_1_STOPBIT | VSF_USART_NO_PARITY
+                            | VSF_USART_TX_ENABLE | VSF_USART_RX_ENABLE,
+        .baudrate           = 921600,
+        .isr                = {
+            .handler_fn     = __vsf_debug_stream_isrhandler,
+            .target_ptr     = &VSF_DEBUG_STREAM_RX,
+            .prio           = vsf_arch_prio_0,
+        },
+    });
+    if (err != VSF_ERR_NONE) {
+        VSF_ASSERT(false);
+        return;
+    }
+
+    while (fsm_rt_cpl != vsf_usart_enable(debug_usart));
+    vsf_usart_irq_enable(debug_usart, VSF_USART_IRQ_MASK_RX);
+#endif
+}
+
+static void __VSF_DEBUG_STREAM_TX_WRITE_BLOCKED(uint8_t *buf, uint_fast32_t size)
+{
+#if 0
+    vsf_usart_t *debug_usart = (vsf_usart_t *)&vsf_hw_usart1;
+    uint_fast16_t cur_size;
+
+    while (size > 0) {
+        cur_size = vsf_usart_txfifo_write(debug_usart, buf, size);
+        if (cur_size > 0) {
+            size -= cur_size;
+            buf += cur_size;
+        }
+    }
+#endif
+}
+
+// Because debug stream for RP2040 is not used,
+//  VSF_HAL_USE_DEBUG_STREAM is not defined in header files.
+// But debug_stream_tx_blocked.inc will need VSF_HAL_USE_DEBUG_STREAM,
+//  so define VSF_HAL_USE_DEBUG_STREAM here.
+#undef VSF_HAL_USE_DEBUG_STREAM
+#define VSF_HAL_USE_DEBUG_STREAM        ENABLED
+#include "hal/driver/common/debug_stream/debug_stream_tx_blocked.inc"
+#endif
+
+void vsf_board_init_linux(void)
+{
+}
+
+void vsf_board_init(void)
+{
+#ifdef __VSF_BOARD_USE_UART_AS_DEBUG_STREAM
+    VSF_STREAM_INIT(&VSF_DEBUG_STREAM_TX);
+#endif
+}
