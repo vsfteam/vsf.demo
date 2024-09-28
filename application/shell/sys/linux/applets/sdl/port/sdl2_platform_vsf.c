160c97a907dccf396db829efb96cdb1e0074a0dc
vsfos <vsfos@foxmail.com>
2024-09-28 16:53:27 +0800

    [application/linux] prepare to add sdl

---
+++ application/shell/sys/linux/applets/sdl/port/sdl2_platform_vsf.c
@@ -0,0 +1,14 @@
+#include "../raw/src/SDL_internal.h"
+
+#ifdef __VSF__
+
+VSF_CAL_NO_INIT SDL_VSF_platform SDL_platform;
+
+void vsf_sdl2_init(vsf_sdl2_cfg_t *cfg)
+{
+    SDL_platform.cfg = *cfg;
+    SDL_platform.is_disp_inited = false;
+    SDL_platform.is_audio_inited = false;
+}
+
+#endif
