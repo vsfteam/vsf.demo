87aacd5dabc23610705ebe292c57a84068fc5aeb
vsfos <vsfos@foxmail.com>
2025-04-06 12:12:23 +0800

    [application/linux/micropython] update CMakeLists.txt

--- application/shell/sys/linux/applets/micropython/micropython.cmake
+++ application/shell/sys/linux/applets/micropython/micropython.cmake
@@ -2,54 +2,54 @@
 
 cmake_policy(SET CMP0079 NEW)
 
-file(GLOB MPY_CORE_FILES raw/py/*.c)
+file(GLOB MPY_CORE_FILES ${MICROPYTHON_PATH}/raw/py/*.c)
 file(GLOB MPY_SOURCES
-    raw/ports/unix/alloc.c
-    raw/ports/unix/coverage.c
-    raw/ports/unix/gccollect.c
-    raw/ports/unix/input.c
-    raw/ports/unix/main.c
-    raw/ports/unix/modsocket.c
-    raw/ports/unix/modtermios.c
-    raw/ports/unix/mpthreadport.c
-    raw/ports/unix/unix_mphal.c
+    ${MICROPYTHON_PATH}/raw/ports/unix/alloc.c
+    ${MICROPYTHON_PATH}/raw/ports/unix/coverage.c
+    ${MICROPYTHON_PATH}/raw/ports/unix/gccollect.c
+    ${MICROPYTHON_PATH}/raw/ports/unix/input.c
+    ${MICROPYTHON_PATH}/raw/ports/unix/main.c
+    ${MICROPYTHON_PATH}/raw/ports/unix/modsocket.c
+    ${MICROPYTHON_PATH}/raw/ports/unix/modtermios.c
+    ${MICROPYTHON_PATH}/raw/ports/unix/mpthreadport.c
+    ${MICROPYTHON_PATH}/raw/ports/unix/unix_mphal.c
 
-    raw/extmod/machine_bitstream.c
-    raw/extmod/machine_i2c.c
-    raw/extmod/machine_mem.c
-    raw/extmod/machine_pinbase.c
-    raw/extmod/machine_pulse.c
-    raw/extmod/machine_pwm.c
-    raw/extmod/machine_signal.c
-    raw/extmod/machine_spi.c
-    raw/extmod/machine_timer.c
-    raw/extmod/modasyncio.c
-    raw/extmod/modbinascii.c
-    raw/extmod/modcryptolib.c
-    raw/extmod/moddeflate.c
-    raw/extmod/modframebuf.c
-    raw/extmod/modhashlib.c
-    raw/extmod/modheapq.c
-    raw/extmod/modjson.c
-    raw/extmod/modmachine.c
-    raw/extmod/modnetwork.c
-#    raw/extmod/modos.c
-    raw/extmod/modplatform.c
-    raw/extmod/modrandom.c
-    raw/extmod/modre.c
-    raw/extmod/modselect.c
-    raw/extmod/modsocket.c
-    raw/extmod/modssl_mbedtls.c
-    raw/extmod/modtime.c
-    raw/extmod/moductypes.c
-    raw/extmod/modwebsocket.c
-    raw/extmod/vfs.c
-    raw/extmod/vfs_posix.c
-    raw/extmod/vfs_posix_file.c
-    raw/extmod/vfs_reader.c
-    raw/extmod/virtpin.c
+    ${MICROPYTHON_PATH}/raw/extmod/machine_bitstream.c
+    ${MICROPYTHON_PATH}/raw/extmod/machine_i2c.c
+    ${MICROPYTHON_PATH}/raw/extmod/machine_mem.c
+    ${MICROPYTHON_PATH}/raw/extmod/machine_pinbase.c
+    ${MICROPYTHON_PATH}/raw/extmod/machine_pulse.c
+    ${MICROPYTHON_PATH}/raw/extmod/machine_pwm.c
+    ${MICROPYTHON_PATH}/raw/extmod/machine_signal.c
+    ${MICROPYTHON_PATH}/raw/extmod/machine_spi.c
+    ${MICROPYTHON_PATH}/raw/extmod/machine_timer.c
+    ${MICROPYTHON_PATH}/raw/extmod/modasyncio.c
+    ${MICROPYTHON_PATH}/raw/extmod/modbinascii.c
+    ${MICROPYTHON_PATH}/raw/extmod/modcryptolib.c
+    ${MICROPYTHON_PATH}/raw/extmod/moddeflate.c
+    ${MICROPYTHON_PATH}/raw/extmod/modframebuf.c
+    ${MICROPYTHON_PATH}/raw/extmod/modhashlib.c
+    ${MICROPYTHON_PATH}/raw/extmod/modheapq.c
+    ${MICROPYTHON_PATH}/raw/extmod/modjson.c
+    ${MICROPYTHON_PATH}/raw/extmod/modmachine.c
+    ${MICROPYTHON_PATH}/raw/extmod/modnetwork.c
+#    ${MICROPYTHON_PATH}/raw/extmod/modos.c
+    ${MICROPYTHON_PATH}/raw/extmod/modplatform.c
+    ${MICROPYTHON_PATH}/raw/extmod/modrandom.c
+    ${MICROPYTHON_PATH}/raw/extmod/modre.c
+    ${MICROPYTHON_PATH}/raw/extmod/modselect.c
+    ${MICROPYTHON_PATH}/raw/extmod/modsocket.c
+    ${MICROPYTHON_PATH}/raw/extmod/modssl_mbedtls.c
+    ${MICROPYTHON_PATH}/raw/extmod/modtime.c
+    ${MICROPYTHON_PATH}/raw/extmod/moductypes.c
+    ${MICROPYTHON_PATH}/raw/extmod/modwebsocket.c
+    ${MICROPYTHON_PATH}/raw/extmod/vfs.c
+    ${MICROPYTHON_PATH}/raw/extmod/vfs_posix.c
+    ${MICROPYTHON_PATH}/raw/extmod/vfs_posix_file.c
+    ${MICROPYTHON_PATH}/raw/extmod/vfs_reader.c
+    ${MICROPYTHON_PATH}/raw/extmod/virtpin.c
 
-    raw/shared/runtime/gchelper_generic.c
+    ${MICROPYTHON_PATH}/raw/shared/runtime/gchelper_generic.c
 
     ${MPY_CORE_FILES}
 )
@@ -70,11 +70,11 @@ vsf_add_compile_definitions(
     VSF_USE_MICROPYTHON=1
 )
 vsf_add_include_directories(
-    port
-    port/${mpy_variant}
-    raw
-    raw/ports/unix
-    raw/ports/unix/variants/${mpy_variant}
+    ${MICROPYTHON_PATH}/port
+    ${MICROPYTHON_PATH}/port/${mpy_variant}
+    ${MICROPYTHON_PATH}/raw
+    ${MICROPYTHON_PATH}/raw/ports/unix
+    ${MICROPYTHON_PATH}/raw/ports/unix/variants/${mpy_variant}
 
     $ENV{VSF_PATH}/source/component/3rd-party/mbedtls/raw/include
 )
