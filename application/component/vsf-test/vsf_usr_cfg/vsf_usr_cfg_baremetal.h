a7a75057231abca3020dcc7a11f7c6654a7297af
Yongxiang Liang <tanekliang@gmail.com>
2026-06-01 17:58:30 +0800

    [application/vsf-test] fix: #ifndef guards in baremetal config; exclude aic8800 CI build

--- application/component/vsf-test/vsf_usr_cfg/vsf_usr_cfg_baremetal.h
+++ application/component/vsf-test/vsf_usr_cfg/vsf_usr_cfg_baremetal.h
@@ -18,13 +18,19 @@
 #ifndef __VSF_USR_CFG_BAREMETAL_H__
 #define __VSF_USR_CFG_BAREMETAL_H__
 
-#define VSF_USE_KERNEL                          DISABLED
+#ifndef VSF_USE_KERNEL
+#   define VSF_USE_KERNEL                          DISABLED
+#endif
 
 // When the kernel is disabled, vsf_os.h (which normally defines these macros)
 // is skipped entirely. queue_stream.c and other service code still reference
 // vsf_protect_scheduler / vsf_unprotect_scheduler, so provide no-op stubs.
-#define vsf_protect_scheduler()                 0
-#define vsf_unprotect_scheduler(__state)        ((void)(__state))
+#ifndef vsf_protect_scheduler
+#   define vsf_protect_scheduler()                 0
+#endif
+#ifndef vsf_unprotect_scheduler
+#   define vsf_unprotect_scheduler(__state)        ((void)(__state))
+#endif
 
 // vsf_kernel_bsp.c defines main() and calls VSF_USER_ENTRY().
 // When the kernel is disabled, define VSF_USER_ENTRY as main so the
